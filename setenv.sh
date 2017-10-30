4951f406d78807c8022b92c17507754df5d4a0b8
Alessandro Bresciani <Alessandro@iMac.local>
2017-10-30 10:32:21 +0000

    preparing heroku deploy

--- setenv.sh
+++
@@ -1,4 +0,0 @@
-export URL="https://api.mailgun.net/v3/sandbox88e3924f2e6a42869ed91bc6d2112c03.mailgun.org/messages"
-export API_KEY="key-7de2af33fead2c883a2034e2e498be60"
-export FROM="Mailgun Sandbox <postmaster@sandbox88e3924f2e6a42869ed91bc6d2112c03.mailgun.org>"
-  
