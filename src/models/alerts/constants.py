221fd18d536f89fb08a0d3e75bdda3b5907bfbfb
Alessandro Bresciani <Alessandro@iMac.local>
2017-10-30 10:30:23 +0000

    preparing heroku deploy

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,5 +1,7 @@
-URL = "https://api.mailgun.net/v3/sandbox88e3924f2e6a42869ed91bc6d2112c03.mailgun.org/messages"
-API_KEY = "key-7de2af33fead2c883a2034e2e498be60"
-FROM = "Mailgun Sandbox <postmaster@sandbox88e3924f2e6a42869ed91bc6d2112c03.mailgun.org>"
-ALERT_TIMEOUT = 1
+import os
+
+URL = os.environ.get('URL')
+API_KEY = os.environ.get('API_KEY')
+FROM = os.environ.get('FROM')
+ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
